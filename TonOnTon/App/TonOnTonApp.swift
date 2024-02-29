3c0382c4852490530f7d1285891fc351f2042d48
mino <kmh922@naver.com>
2024-02-29 13:23:37 +0900

    refactor: rootView 수정

--- TonOnTon/App/TonOnTonApp.swift
+++ TonOnTon/App/TonOnTonApp.swift
@@ -5,13 +5,14 @@
 //  Created by mino on 2024/02/29.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TonOnTonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            IntroView(store: Store(initialState: .init(), reducer: { IntroFeature() }))
         }
     }
 }
