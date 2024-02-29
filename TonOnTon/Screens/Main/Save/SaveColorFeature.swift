5b49f5eef9315ecae2351332198cd523c551de2f
mino <kmh922@naver.com>
2024-02-29 11:23:18 +0900

    feat: TabBar View 구성

---
+++ TonOnTon/Screens/Main/Save/SaveColorFeature.swift
@@ -0,0 +1,24 @@
+//
+//  SaveColorFeature.swift
+//  TonOnTon
+//
+//  Created by mino on 2024/02/29.
+//
+
+import ComposableArchitecture
+
+struct SaveColorFeature: Reducer {
+    struct State: Equatable {}
+    
+    enum Action: Equatable {
+
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}
