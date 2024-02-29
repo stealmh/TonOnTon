f926d50ebcd1f8726121fa53abb8fdb9a7c2c07d
mino <kmh922@naver.com>
2024-02-29 17:12:00 +0900

    feat: Font 추가

---
+++ TonOnTon/Model/CreateColor.swift
@@ -0,0 +1,14 @@
+//
+//  CreateColor.swift
+//  TonOnTon
+//
+//  Created by mino on 2024/02/29.
+//
+
+import SwiftUI
+
+struct CreateColor: Hashable {
+    let shirtColor: Color
+    let shirtPointCOlor: Color?
+    let pantsColor: Color
+}
