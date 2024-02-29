18dfdee0729b3b0a542ea4c87c6db7e67269136a
mino <kmh922@naver.com>
2024-02-29 13:23:59 +0900

    feat: TabBarFeature 추가

--- TonOnTon/Screens/Tabbar/TabBarFeature.swift
+++ TonOnTon/Screens/Tabbar/TabBarFeature.swift
@@ -13,15 +13,34 @@ struct TabBarFeature: Reducer {
     
     struct State: Equatable {
         var selectedTab: Tab = .home
+        
+        var home = HomeFeature.State()
+        var saveColor = SaveColorFeature.State()
     }
     
-    enum Action: Equatable {}
+    enum Action: Equatable {
+        case home(HomeFeature.Action)
+        case saveColor(SaveColorFeature.Action)
+        case tabSelected(Tab)
+    }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-                
+            case .home:
+                return .none
+            case .saveColor:
+                return .none
+            case .tabSelected(let tab):
+                state.selectedTab = tab
+                return .none
             }
+        }
+        Scope(state: \.home, action: /TabBarFeature.Action.home) {
+            HomeFeature()
+        }
+        Scope(state: \.saveColor, action: /TabBarFeature.Action.saveColor) {
+            SaveColorFeature()
         }
     }
 }
