ac5d3c226a81389605d81611827bfefc0269fbe0
mino <kmh922@naver.com>
2024-02-29 09:46:04 +0900

    Initial Commit

---
+++ TonOnTon/TonOnTonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonOnTonApp.swift
+//  TonOnTon
+//
+//  Created by DEV IOS on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct TonOnTonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
