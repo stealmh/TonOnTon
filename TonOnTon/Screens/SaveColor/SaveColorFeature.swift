dfa0b28de3ced4cece2b3a0471272996bac9ade3
mino <kmh922@naver.com>
2024-02-29 14:54:06 +0900

    feat: SaveColor의 State로 AddColor 추가

--- TonOnTon/Screens/SaveColor/SaveColorFeature.swift
+++ TonOnTon/Screens/SaveColor/SaveColorFeature.swift
@@ -8,17 +8,43 @@
 import ComposableArchitecture
 
 struct SaveColorFeature: Reducer {
-    struct State: Equatable {}
+    
+    struct Destination: Reducer {
+        enum State: Equatable {
+            case addColor(AddColorFeature.State = AddColorFeature.State())
+        }
+        enum Action: Equatable {
+            case addColor(AddColorFeature.Action)
+        }
+        var body: some ReducerOf<Destination> {
+            Scope(state: /State.addColor, action: /Action.addColor) {
+                AddColorFeature()
+            }
+        }
+    }
+    
+    struct State: Equatable {
+        @PresentationState var destination: Destination.State?
+    }
     
     enum Action: Equatable {
-
+        case addButtonTapped
+        case destination(PresentationAction<Destination.Action>)
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-                
+            case .addButtonTapped:
+                state.destination = .addColor()
+                return .none
+            case .destination:
+                return .none
             }
         }
+        .ifLet(\.$destination, action: /Action.destination) {
+            Destination()
+        }
+        ._printChanges()
     }
 }
--- TonOnTon/Screens/SaveColor/SaveColorView.swift
+++ TonOnTon/Screens/SaveColor/SaveColorView.swift
@@ -21,7 +21,27 @@ struct SaveColorView: View {
 //MARK: - View
 extension SaveColorView {
     var body: some View {
-        Text("Hello, this is Save Color View!")
+        NavigationStack {
+            VStack {
+                Text("Hello, this is Save Color View!")
+            }
+            .toolbar {
+                Button(action: { viewStore.send(.addButtonTapped) }) {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(
+              store: self.store.scope(state: \.$destination, action: { .destination($0) }),
+              state: /SaveColorFeature.Destination.State.addColor,
+              action: SaveColorFeature.Destination.Action.addColor
+            ) { store in
+                AddColorView(store: store)
+                    .presentationDetents([.fraction(0.9)])
+//                    .interactiveDismissDisabled()
+    //                .presentationBackground(.gray.opacity(0.6))
+            }
+
+        }
     }
 }
 //MARK: - Preview
