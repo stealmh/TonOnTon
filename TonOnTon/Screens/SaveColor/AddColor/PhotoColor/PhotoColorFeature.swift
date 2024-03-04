071a4778f3cbfd57d96579ee88a1e999395ebf87
mino <kmh922@naver.com>
2024-03-04 16:58:03 +0900

    feat: 사진에서 컬러 추출 Feature & View 추가

---
+++ TonOnTon/Screens/SaveColor/AddColor/PhotoColor/PhotoColorFeature.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoColorFeature.swift
+//  TonOnTon
+//
+//  Created by mino on 2024/03/04.
+//
+
+import ComposableArchitecture
+
+struct PhotoColorFeature: Reducer {
+    struct State: Equatable {}
+    enum Action: Equatable {}
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {}
+    }
+}
---
+++ TonOnTon/Screens/SaveColor/AddColor/PhotoColor/PhotoColorSelectView.swift
@@ -0,0 +1,139 @@
+//
+//  PhotoColorSelectView.swift
+//  TonOnTon
+//
+//  Created by mino on 2024/03/04.
+//
+
+import ComposableArchitecture
+import PhotosUI
+import SwiftUI
+
+struct PhotoColorSelectView: View {
+    /// - Properties
+    private let store: StoreOf<PhotoColorFeature>
+    @ObservedObject var viewStore: ViewStoreOf<PhotoColorFeature>
+    @State var colorArray:[UIColor] = []
+    @State private var selectedItem: PhotosPickerItem?
+    @State var selectedImage: Data? = nil
+    @State private var myImage: Image?
+    @State private var myImage2: UIImage?
+    @State private var colorPicker: Color = .white
+    @State private var userselectColors: [UIColor] = []
+    
+    let columns:[GridItem] = [GridItem(.flexible(), spacing: nil, alignment: .center),GridItem(.flexible(), spacing: nil, alignment: .center),GridItem(.flexible(), spacing: nil, alignment: .center)]
+    
+    init(store: StoreOf<PhotoColorFeature>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store, observe: { $0 })
+    }
+}
+//MARK: - View
+extension PhotoColorSelectView {
+    var body: some View {
+
+            ZStack {
+                VStack {
+                    HStack {
+                        PhotosPicker("Select an image", selection: $selectedItem, matching: .images)
+                        Spacer()
+                        CustomColorPicker(color: $colorPicker)
+                            .frame(width: 100, height: 50)
+                            .clipped()
+                            .padding(10)
+                    }
+                    
+                        .onChange(of: selectedItem) { newItem in
+                            Task {
+                                if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                    selectedImage = data
+                                    guard let selectedImage, let uiImage = UIImage(data: selectedImage) else { return }
+                                    myImage = Image(uiImage: uiImage)
+                                    myImage2 = uiImage
+                                }
+                                print("Failed to load the image")
+                            }
+                        }
+                    
+                    Image(uiImage: myImage2 ?? UIImage())
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(16)
+                        .frame(maxWidth: .infinity).frame(height: 250).padding(.horizontal,8)
+                  
+                    Spacer()
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: columns) {
+                            ForEach(colorArray,id: \.self) { color in
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(userselectColors.contains(color) ? .red : .clear, lineWidth: 2)
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 45)
+                                    .padding(.horizontal,4)
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .foregroundColor(Color(uiColor: color))
+                                            .padding(.horizontal,4)
+                                    }
+                                    .overlay(alignment: .center) {
+                                        HStack {
+                                            Text(color.hexString)
+                                        }.font(.caption).foregroundColor(.white)
+                                    }
+                                    .onTapGesture {
+                                        if !userselectColors.contains(color) && userselectColors.count < 2 {
+                                            userselectColors.append(color)
+                                        } else {
+                                            if userselectColors.count == 2 && userselectColors.contains(color) {
+                                                userselectColors.removeAll { $0 == color }
+                                            }
+                                            
+                                            if userselectColors.count == 2 && !userselectColors.contains(color) {
+                                                /// animation: 흔들흔들
+                                            }
+                                            else {
+                                                userselectColors.removeAll { $0 == color }
+                                            }
+                                        }
+                                    }
+                            }
+                    
+                        }
+                    }
+                    
+                    Spacer()
+                    Button(action: {}) {
+                        Text("다음")
+                    }
+                    .opacity(userselectColors.count == 2 ? 1 : 0)
+                }
+                .onChange(of: myImage2, perform: { uiImage in
+                    guard let colors = myImage2?.dominantColors() else {return}
+                    colorArray.removeAll()
+                    for color in colors {
+                        colorArray.append(color)
+                    }
+                    let setColor = Array(Set(colorArray))
+                    let sortedColors = setColor.sorted { $0.brightness > $1.brightness }
+                    colorArray = sortedColors
+                })
+                .onChange(of: colorPicker) { newValue in
+                    let spoidColor = UIColor(newValue)
+                    colorArray.append(spoidColor)
+                }
+            }
+            .toolbar(.hidden, for: .tabBar)
+    }
+}
+//MARK: - Preview
+struct PhotoColorSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoColorSelectView(
+            store: Store(
+                initialState: .init(),
+                reducer: { PhotoColorFeature() }
+            )
+        )
+    }
+}
