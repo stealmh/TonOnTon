db2fa21a4919e9bddfb3be0b49b1b89485373498
mino <kmh922@naver.com>
2024-03-11 17:20:38 +0900

    refactor: CreateColor Idenditied하게 수정

--- TonOnTon/Model/CreateColor.swift
+++ TonOnTon/Model/CreateColor.swift
@@ -5,10 +5,11 @@
 //  Created by mino on 2024/02/29.
 //
 
+import IdentifiedCollections
 import SwiftUI
 
-struct CreateColor: Hashable {
+struct CreateColor: Equatable, Identifiable {
+    let id: UUID
     let shirtColor: Color
-    let shirtPointCOlor: Color?
     let pantsColor: Color
 }
